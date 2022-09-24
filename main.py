881c706a33f5fa056c81bd08d991449a912ad642
itsparasbisht <iamparasbisht@gmail.com>
2022-09-24 20:30:38 +0530

    joinedDict

--- main.py
+++ main.py
@@ -7,12 +7,9 @@ class ConnectionManager:
     def __init__(self):
         self.active_connections: List[WebSocket] = []
 
-    async def connect(self, websocket: WebSocket, client):
+    async def connect(self, websocket: WebSocket):
         await websocket.accept()
         self.active_connections.append(websocket)
-
-        for connection in self.active_connections:
-            await connection.send_text(client)
 
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
@@ -28,13 +25,24 @@ manager = ConnectionManager()
 
 @app.websocket("/ws/{client}")
 async def websocket_endpoint(websocket: WebSocket, client: str):
-    await manager.connect(websocket, client)
+    await manager.connect(websocket)
+    joinedDict = {
+        "joined" : True,
+        "message" :  f'"{client.upper()} has joined the chat"'
+    }
+    await manager.broadcast(joinedDict)
+
     try:
         while True:
             data = await websocket.receive_json()
+            print(data)
             await manager.broadcast(data)
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client} left the chat")
+        leftDict = {
+            "left" : True,
+            "message" :  f'"{client.upper()} left the chat"'
+        }
+        await manager.broadcast(leftDict)
 
 
